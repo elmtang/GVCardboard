470d4bb91b1109c99e52971b4e3d667a9302c480
elmtang <hao.tang@gmail.com>
2021-10-26 09:15:08 -0400

    update start function

--- Assets/cardboard-xr-plugin-feature-xr_interaction/Samples/hellocardboard-unity/Scripts/TestVRObj.cs
+++ Assets/cardboard-xr-plugin-feature-xr_interaction/Samples/hellocardboard-unity/Scripts/TestVRObj.cs
@@ -7,7 +7,7 @@ public class TestVRObj : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //update Start function
     }
 
     // Update is called once per frame
